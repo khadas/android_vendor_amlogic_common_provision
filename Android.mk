4535338f4985155d794bc865e5c207ad8b2a0348
Liqiang Jin <liqiang.jin@amlogic.com>
2017-12-14 12:29:57 +0800

    support hdcp rx14 key provision
    Change-Id: I4892b0729dd95ec2ae81e08056f9a6b0e9a59004
    Signed-off-by: Liqiang Jin <liqiang.jin@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -10,7 +10,22 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+ifeq ($(TARGET_ENABLE_TA_SIGN), true)
+ifeq ($(TARGET_ENABLE_TA_ENCRYPT), true)
+ENCRYPT := --encrypt=1
+else
+ENCRYPT := --encrypt=0
+endif
+$(info $(shell mkdir $(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/ta/signed))
+$(info $(shell $(ANDROID_BUILD_TOP)/vendor/amlogic/tdk/ta_export/scripts/sign_ta_auto.py \
+		--in=$(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/ta/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
+		--out=$(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/ta/signed/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
+		$(ENCRYPT)))
+LOCAL_SRC_FILES := ta/signed/e92a43ab-b4c8-4450-aa12b1516259613b.ta
+else
 LOCAL_SRC_FILES := ta/e92a43ab-b4c8-4450-aa12b1516259613b.ta
+endif
+
 LOCAL_MODULE := e92a43ab-b4c8-4450-aa12b1516259613b
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .ta
@@ -19,11 +34,9 @@ LOCAL_STRIP_MODULE := false
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS += -DANDROID_BUILD
-LOCAL_CFLAGS += -Wall
-LOCAL_SRC_FILES += ca/src/provision.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/ca/include
-LOCAL_SHARED_LIBRARIES := libteec libprovision
-LOCAL_MODULE := tee_provision
 LOCAL_MODULE_TAGS := optional
-include $(BUILD_EXECUTABLE)
+LOCAL_SRC_FILES := ca/tee_provision
+LOCAL_MODULE := tee_provision
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/bin
+include $(BUILD_PREBUILT)
