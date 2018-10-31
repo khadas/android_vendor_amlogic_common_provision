9703bfde81105aa81cf0e980e1d539bb74a42cb6
Hangyu Li <hangyu.li@amlogic.com>
2018-10-31 11:04:33 +0800

    provision: move provision ta/ca/lib to vendor directory [4/4]
    PD#SWPL-1199
    
    Problem:
    provision need locate in vendor dir in android treble mode
    
    Solution:
    move provision related files to vendor directory
    
    Verify:
    Android P + Ampere
    
    Change-Id: Iabfec1c57747e1f3f313d7744562ebe89aba0e1a
    Signed-off-by: Hangyu Li <hangyu.li@amlogic.com>

--- ca/Android.mk
+++ ca/Android.mk
@@ -1,5 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+OUT_PATH := $(PRODUCT_OUT)/vendor
+else
+OUT_PATH := $(PRODUCT_OUT)/system
+endif
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES_32 := lib/libprovision.so
 LOCAL_SRC_FILES_64 := lib64/libprovision.so
@@ -7,6 +13,10 @@ LOCAL_MODULE := libprovision
 LOCAL_MULTILIB := both
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -20,5 +30,5 @@ endif
 
 LOCAL_MODULE := tee_provision
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/vendor/bin
+LOCAL_MODULE_PATH := $(OUT_PATH)/bin
 include $(BUILD_PREBUILT)
--- ta/Android.mk
+++ ta/Android.mk
@@ -1,5 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+OUT_PATH := $(PRODUCT_OUT)/vendor
+else
+OUT_PATH := $(PRODUCT_OUT)/system
+endif
+
 include $(CLEAR_VARS)
 ifeq ($(TARGET_ENABLE_TA_SIGN), true)
 ifeq ($(TARGET_ENABLE_TA_ENCRYPT), true)
@@ -7,11 +13,19 @@ ENCRYPT := --encrypt=1
 else
 ENCRYPT := --encrypt=0
 endif
-$(info $(shell mkdir $(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/signed))
-$(info $(shell $(ANDROID_BUILD_TOP)/vendor/amlogic/tdk/ta_export/scripts/sign_ta_auto.py \
-		--in=$(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
-		--out=$(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/signed/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
-		$(ENCRYPT)))
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 28 && echo OK),OK)
+KEYDIR := --keydir=$(shell pwd)/$(BOARD_AML_TDK_KEY_PATH)
+else
+KEYDIR := --keydir=$(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/keys/
+endif
+$(info $(shell mkdir $(shell pwd)/$(LOCAL_PATH)/signed))
+$(info $(shell $(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/scripts/sign_ta_auto.py \
+		--in=$(shell pwd)/$(LOCAL_PATH)/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
+		--out=$(shell pwd)/$(LOCAL_PATH)/signed/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
+		$(ENCRYPT) \
+		$(KEYDIR)))
+
 LOCAL_SRC_FILES := signed/e92a43ab-b4c8-4450-aa12b1516259613b.ta
 else
 LOCAL_SRC_FILES := e92a43ab-b4c8-4450-aa12b1516259613b.ta
@@ -20,7 +34,7 @@ endif
 LOCAL_MODULE := e92a43ab-b4c8-4450-aa12b1516259613b
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .ta
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/lib/teetz
+LOCAL_MODULE_PATH := $(OUT_PATH)/lib/teetz
 LOCAL_STRIP_MODULE := false
 include $(BUILD_PREBUILT)
 
