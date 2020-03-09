5f2fbf6451deb3ff7977fe8815bf0c7afa5853b6
jie.yuan <jie.yuan@amlogic.com>
2020-03-09 11:57:50 +0800

    sabrina: fix build failed on branch q-tv-dev [1/1]
    PD#SWPL-20761
    
    Problem:
    Sabrina build failed
    because Sabrina has itself hdcp_ta and provision_ta
    under vendor/google_devices/sabrina
    and it doesn't need use the common one
    
    Solution:
    do not build it when target is sabrina
    
    Verify:
    Sabrina
    
    Signed-off-by: jie.yuan <jie.yuan@amlogic.com>
    Change-Id: I991be5244004de05509d0c9cbcd317617642bea7

--- Android.mk
+++ Android.mk
@@ -1,2 +1,4 @@
-include $(call all-subdir-makefiles)
-#include $(call all-named-subdir-makefiles, ca ta)
+ifeq (,$(filter $(TARGET_DEVICE),sabrina))
+  include $(call all-subdir-makefiles)
+  #include $(call all-named-subdir-makefiles, ca ta)
+endif
