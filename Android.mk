e247be42a6a5b20c6cc2be96a5cfbc6eb9703d9e
yidong zhang <yidong.zhang@amlogic.com>
2021-04-08 10:49:18 +0800

    Boreal: boreal using its own provision bin [1/1]
    PD#GH-929
    
    Problem:
    boreal used the provision ca & ta with itself.
    
    Solution:
    remove provision boreal from provision project.
    
    Verify:
    Buildroot
    
    Signed-off-by: yidong zhang <yidong.zhang@amlogic.com>
    Change-Id: I9b1c0ff60584868f7c28842949d65a3cd2f0dec6

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq (,$(filter $(TARGET_DEVICE),sabrina))
+ifeq (,$(filter $(TARGET_DEVICE),sabrina, boreal))
   include $(call all-subdir-makefiles)
   #include $(call all-named-subdir-makefiles, ca ta)
 endif
