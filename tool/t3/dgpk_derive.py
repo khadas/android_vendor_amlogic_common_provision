976b199eb5ef33da900ef4f4fe7e405d099e58f0
Liqiang Jin <liqiang.jin@amlogic.com>
2021-06-19 16:13:28 +0800

    Efuse: need Efuse tool for T3 [1/1]
    PD#SWPL-53049
    
    Problem:
    need Efuse tool for T3
    
    Solution:
    add tool
    
    Verify:
    Android R + T3
    
    Change-Id: Id007c67e2d6ca3e6e75ba329206bc2109e8c1253
    Signed-off-by: Liqiang Jin <liqiang.jin@amlogic.com>

---
+++ tool/t3/dgpk_derive.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2016 Amlogic, Inc. All rights reserved.
+#
+#
+# This source code is subject to the terms and conditions defined in the
+# file 'LICENSE' which is part of this source code package.
+#
+#
+
+# dgpk: device global protect key
+def main():
+	import os
+	import random
+	import uuid
+	from Crypto.Hash import SHA256
+
+	uuid = uuid.uuid4()
+	sha = SHA256.new()
+	sha.update(uuid.bytes_le)
+
+	dgpk1 = sha.digest()[:16]
+	dgpk2 = sha.digest()[16:]
+
+	f = open("dgpk1.bin", 'wb')
+	f.write(dgpk1)
+	f.close()
+
+	f = open("dgpk2.bin", 'wb')
+	f.write(dgpk2)
+	f.close()
+
+	print 'Generating DGPK1 and DGPK2 ...'
+	print 'Output:    DGPK1(dgpk1.bin) = ' + sha.hexdigest()[:32]
+	print '           DGPK2(dgpk2.bin) = ' + sha.hexdigest()[32:]
+
+if __name__ == "__main__":
+	main()
---
+++ tool/t3/pcpk_derive.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2016 Amlogic, Inc. All rights reserved.
+#
+#
+# This source code is subject to the terms and conditions defined in the
+# file 'LICENSE' which is part of this source code package.
+#
+#
+
+import sys
+
+CHIP = "t3"
+VENDOR_KEYTOOL = sys.path[0] + "/../../../../../../bootloader/uboot-repo/fip/" + CHIP + "/binary-tool/vendor-keytool"
+ALGO = "gen-prot-aes128"
+MRK_NAME = "DGPK1"
+BOOT_STAGE = 2
+EK3 = "0f3edbdb962f84c4c442defca2ce8c35"
+EK2 = "bef32c3e35bccde2525bf7e780088760"
+EK1 = "9146456a8c11c3ae180cc148372ac64b"
+
+def get_args():
+	from argparse import ArgumentParser
+
+	parser = ArgumentParser()
+	parser.add_argument('--dgpk1', type = str, required = True, \
+			help = 'dgpk1 string')
+
+	return parser.parse_args()
+
+def main():
+	import os
+	import sys
+	import binascii
+
+	if not os.path.exists(VENDOR_KEYTOOL):
+		print VENDOR_KEYTOOL + " not exist"
+		sys.exit(1)
+
+	args = get_args()
+	mrk = args.dgpk1
+
+	# due to the parameter "T3" aren't supported, can only use "SC2"
+	cmd = VENDOR_KEYTOOL + " " + ALGO + " --chipset=" + "SC2" + " --mrk=" \
+	      + mrk + " --mrk-name=" + MRK_NAME + " --boot-stage=" \
+		+ str(BOOT_STAGE) + " --ek3=" + EK3 + " --ek2=" + EK2 \
+		+ " --ek1=" + EK1
+
+	pcpk = os.popen(cmd).read()
+
+	f = open(CHIP + "_pcpk.bin", 'wb')
+	f.write(binascii.unhexlify(pcpk[:32]))
+	f.close()
+
+	print 'Generating PCPK ...'
+	print "  Input: DGPK1 = " + args.dgpk1
+	print "  Output: PCPK(" + CHIP + "_pcpk.bin)" + " = " + pcpk
+
+if __name__ == "__main__":
+	main()
