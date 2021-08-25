a20e21310c6f68b59387349875d7fcb21dff31ad
Liqiang Jin <liqiang.jin@amlogic.com>
2021-08-25 11:37:35 +0800

    tool: add provision_keywrapper/sha256_generator dynamic link library [1/1]
    PD#SWPL-56569
    
    Problem:
    need to add provision_keywrapper/sha256_generator dynamic link library
    
    Solution:
    modify code
    
    Verify:
    PC
    
    Change-Id: I4c25558161a87104d9f667f4e89c04abec200c10
    Signed-off-by: Liqiang Jin <liqiang.jin@amlogic.com>

---
+++ tool/dynamic-lib-tool/sha256-generator/include/sha256_generator.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2014-2017 Amlogic, Inc. All rights reserved.
+ *
+ * All information contained herein is Amlogic confidential.
+ *
+ * This software is provided to you pursuant to Software License Agreement
+ * (SLA) with Amlogic Inc ("Amlogic"). This software may be used
+ * only in accordance with the terms of this agreement.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification is strictly prohibited without prior written permission from
+ * Amlogic.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _SHA256_GENERATOR_H_
+#define _SHA256_GENERATOR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* sha256_buf: output parameter, sha256 data buffer;
+   buf_size: input/output parameter,
+             input: buffer size(min value: 32),
+             output: sha256 size;
+   data: input parameter, data;
+   data_size: input parameter, data size;
+
+   return value: 0: success; non 0: failed;
+ */
+int generate_sha256(
+		uint8_t *sha256_buf,
+		uint32_t *buf_size,
+		const uint8_t *data,
+		uint32_t data_size
+		);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SHA256_GENERATOR_H_ */
