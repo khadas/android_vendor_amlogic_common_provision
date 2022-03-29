7b1c5f35e6dabeb77dbd8620ab8bba1ea12f3924
Liqiang Jin <liqiang.jin@amlogic.com>
2022-03-29 14:53:12 +0800

    Provision: dac error [1/1]
    PD#SWPL-76424
    
    Problem:
    dac error
    
    Solution:
    update python script
    
    Verify:
    manual
    
    Change-Id: Icc21ab108b628bfffa93f856d92be59d2512b4c0
    Signed-off-by: Liqiang Jin <liqiang.jin@amlogic.com>

--- tool/dev_auth_code_derive.py
+++ tool/dev_auth_code_derive.py
@@ -1,56 +1,88 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2016 Amlogic, Inc. All rights reserved.
-#
+# Copyright (C) 2022 Amlogic, Inc. All rights reserved.
 #
 # This source code is subject to the terms and conditions defined in the
 # file 'LICENSE' which is part of this source code package.
-#
-#
+
+import sys
+
+GENERATOR_PATH = sys.path[0] + '/dependents/pfpk-hmac-generator/pfpk_hmac_generator'
+
+TARGET_NAME = 'dac'
+MAX_IDX = 100000000
+DERIVED_FILE_NUM = 0
 
 def get_args():
 	from argparse import ArgumentParser
 
 	parser = ArgumentParser()
-	parser.add_argument('--pfpk', required = True, \
-			help = 'provision field protect key file')
-	parser.add_argument('--pfid', required = True, \
-			help = 'provision field id')
-	parser.add_argument('--dac', type = str, default = 'dac.bin', \
-			help = 'device authentication code file(output)')
-
+	parser.add_argument('--pfpk', type = str, required = True, help = 'pfpk file or directory')
+	parser.add_argument('--pfid', type = str, required = True, help = 'pfid file or directory')
+	parser.add_argument('--out_dir', type = str, default = './dacs/', help = 'output directory')
 	return parser.parse_args()
 
-# pfpk:		provision field protect key
-# pfid:		provision field id
-# dac:		device authentication code
-
-def main():
+def derive_one_dac(file_pfpk, file_pfid, out_dir, idx):
 	import os
+	import sys
+	import binascii
 	import hmac
 	import hashlib
-	import struct
 
-	args = get_args()
+	if int(idx) > MAX_IDX:
+		print('the number of files exceeds the limit')
+		sys.exit(1)
 
-	f = open(args.pfpk, 'rb')
-	pfpk = f.read(16)
+	f = open(file_pfpk, 'rb')
+	pfpk = f.read()
 	f.close()
 
-	f = open(args.pfid, 'rb')
-	pfid = f.read(16)
+	f = open(file_pfid, 'rb')
+	pfid = f.read()
 	f.close()
 
-	hmac_key = pfpk + struct.pack('<4I', *([0] * 4))
-	dac = hmac.HMAC(hmac_key, pfid, hashlib.sha256).digest()
-	f = open(args.dac, 'wb')
+	cmd = GENERATOR_PATH + ' -p ' + bytes.decode(binascii.b2a_hex(pfpk))
+	pfpk_hmac = binascii.unhexlify((os.popen(cmd).read())[:64])
+
+	dac = hmac.HMAC(pfpk_hmac, pfid, hashlib.sha256).digest()
+
+	file_dac = out_dir + '/' + TARGET_NAME + '_' + idx + '.bin'
+	f = open(file_dac, 'wb')
 	f.write(dac)
 	f.close()
 
-	print ('Device Authentication Code Derive ...')
-	print ('  Input:                 pfpk file = ' + args.pfpk)
-	print ('                         pfid file = ' + args.pfid)
-	print ('  Output:                 dac file = ' + args.dac)
+	global DERIVED_FILE_NUM
+	DERIVED_FILE_NUM = DERIVED_FILE_NUM + 1
+	print(os.path.abspath(file_dac) + ' derived successfully')
 
-if __name__ == "__main__":
+def main():
+	import os
+	import sys
+
+	args = get_args()
+
+	if not os.path.exists(GENERATOR_PATH):
+		print(GENERATOR_PATH + ' not exist')
+		sys.exit(1)
+
+	if not os.path.exists(args.out_dir):
+		os.mkdir(args.out_dir)
+
+	if os.path.isfile(args.pfpk) and os.path.isfile(args.pfid):
+		derive_one_dac(args.pfpk, args.pfid, args.out_dir, '000000001')
+	elif os.path.isdir(args.pfpk) and os.path.isdir(args.pfid):
+		for parent, dnames, fnames in os.walk(args.pfpk):
+			for fname in fnames:
+				idx = fname[-13 : -4]
+				file_pfid = args.pfid + '/' + 'pfid_' + idx + '.bin'
+				file_pfpk = os.path.join(parent, fname)
+				if os.path.exists(file_pfid):
+					derive_one_dac(file_pfpk, file_pfid, args.out_dir, idx)
+	else:
+		print('input parameter error')
+		sys.exit(1)
+
+	print(str(DERIVED_FILE_NUM) + ' ' + TARGET_NAME + ' files derived successfully')
+
+if __name__ == '__main__':
 	main()
--- tool/provision_key_derive.py
+++ tool/provision_key_derive.py
@@ -1,79 +1,70 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2016 Amlogic, Inc. All rights reserved.
-#
+# Copyright (C) 2022 Amlogic, Inc. All rights reserved.
 #
 # This source code is subject to the terms and conditions defined in the
 # file 'LICENSE' which is part of this source code package.
-#
-#
+
+MAX_IDX = 100000000
+KEY_SIZE = 16
 
 def get_args():
 	from argparse import ArgumentParser
 
 	parser = ArgumentParser()
-	parser.add_argument('--model_id', type = int, default = 0, \
-			help = 'model id')
-	parser.add_argument('--sub_id', type = int, default = 0, \
-			help = 'sub id')
-	parser.add_argument('--counter_base', type = int, default = 0, \
-			help = 'counter base')
-	parser.add_argument('--count', type = int, required = True, \
-			help = 'device count')
-	parser.add_argument('--out_dir', type = str, default = './', \
-			help = 'output directory of the derived key files')
+	parser.add_argument('--model_id', type = int, default = 0, help = 'model id')
+	parser.add_argument('--sub_id', type = int, default = 0, help = 'sub id')
+	parser.add_argument('--counter_base', type = int, default = 0, help = 'counter base')
+	parser.add_argument('--count', type = int, required = True, help = 'device count')
+	parser.add_argument('--out_dir', type = str, default = './provision-keys/', help = 'output directory of the derived key files')
 
 	return parser.parse_args()
 
-def derive_random():
-	import uuid
-	from hashlib import md5
-
-	md = md5()
-	md.update(uuid.uuid4().bytes_le)
-	return md.digest()
-
-def make_file_path(file_dir, pre_file_name, file_no, need_no):
-	if need_no == True:
-		return file_dir + "/" + pre_file_name + "-" + str(file_no) \
-		+ ".bin"
-	else:
-		return file_dir + "/" + pre_file_name + ".bin"
-
 def derive_key_files(key_cnt, key_name, out_dir):
 	import struct
+	import os
+
+	if not os.path.exists(out_dir):
+		os.mkdir(out_dir)
 
 	for i in range(1, key_cnt + 1):
-		f = open(make_file_path(out_dir, key_name, i, key_cnt > 1), \
-				'wb')
-		f.write(derive_random())
+		file_path = out_dir + '/' + key_name + '_' + str(i).zfill(9) + '.bin'
+		f = open(file_path, 'wb')
+		f.write(os.getrandom(KEY_SIZE))
 		f.close()
+		print(os.path.abspath(file_path) + ' derived successfully')
 
 def derive_pcpk_file(out_dir):
-	derive_key_files(1, "pcpk", out_dir)
+	derive_key_files(1, 'pcpk', out_dir + '/pcpk/')
 
 def derive_pek_files(key_cnt, out_dir):
-	derive_key_files(key_cnt, "pek", out_dir)
+	derive_key_files(key_cnt, 'pek', out_dir + '/peks/')
 
 def derive_pfid_files(model_id, sub_id, counter_base, id_cnt, out_dir):
 	import numpy
 	import struct
+	import os
+
+	out_dir = out_dir + '/pfids/'
+	if not os.path.exists(out_dir):
+		os.mkdir(out_dir)
 
 	for i in range(1, id_cnt + 1):
-		f = open(make_file_path(out_dir, "pfid", i, id_cnt > 1), 'wb')
-		f.write(struct.pack('I', model_id))
-		f.write(struct.pack('I', sub_id))
-		f.write(struct.pack('Q', numpy.int64(counter_base + i)))
+		file_path = out_dir + '/pfid_' + str(i).zfill(9) + '.bin'
+		f = open(file_path, 'wb')
+		f.write(struct.pack('>I', model_id))
+		f.write(struct.pack('>I', sub_id))
+		f.write(struct.pack('>Q', numpy.int64(counter_base + i)))
 		f.close()
+		print(os.path.abspath(file_path) + ' derived successfully')
 
 def derive_pfpk_files(key_cnt, out_dir):
-	derive_key_files(key_cnt, "pfpk", out_dir)
+	derive_key_files(key_cnt, 'pfpk', out_dir + '/pfpks/')
 
 # pcpk:		provision common protect key
 # pek:		provision encrypt key
 # pfpk:		provision field protect key
 # pfid:		provision field id
-
 def main():
 	import os
 
@@ -82,31 +73,14 @@ def main():
 	if not os.path.exists(args.out_dir):
 		os.mkdir(args.out_dir)
 
+	if args.count > MAX_IDX:
+		args.count = MAX_IDX
+		print('the max number of device is ' + MAX_IDX)
+
 	derive_pcpk_file(args.out_dir)
 	derive_pek_files(args.count, args.out_dir)
-	derive_pfid_files(args.model_id, args.sub_id, args.counter_base, \
-			args.count, args.out_dir)
+	derive_pfid_files(args.model_id, args.sub_id, args.counter_base, args.count, args.out_dir)
 	derive_pfpk_files(args.count, args.out_dir)
 
-	print ('Provision Key Derive ...')
-	print ('  Input:                 model_id = ' + str(args.model_id))
-	print ('                           sub_id = ' + str(args.sub_id))
-	print ('                     counter_base = ' + str(args.counter_base))
-	print ('                            count = ' + str(args.count))
-	print ('                          out_dir = ' + args.out_dir)
-	if args.count == 1:
-		print ('  Output:               pcpk file = pcpk.bin')
-		print ('                         pek file = pek.bin')
-		print ('                        pfid file = pfid.bin')
-		print ('                        pfpk file = pfpk.bin')
-	else:
-		print ('  Output:               pcpk file = pcpk.bin')
-		print ('                         pek file = pek-{1...' \
-			+ str(args.count) + '}.bin')
-		print ('                        pfid file = pfid-{1...' \
-			+ str(args.count) + '}.bin')
-		print ('                        pfpk file = pfpk-{1...' \
-			+ str(args.count) + '}.bin')
-
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
